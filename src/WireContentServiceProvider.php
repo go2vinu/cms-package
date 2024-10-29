18cca9fbef3ffc19683b3e1e32c86567b4917c40
go2vinu <go2vinu@users.noreply.github.com>
2024-10-29 07:06:06 +0000

    Fix styling

--- src/WireContentServiceProvider.php
+++ src/WireContentServiceProvider.php
@@ -4,7 +4,6 @@ namespace WireContent;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use WireContent\Commands\InstallCommand;
 
 class WireContentServiceProvider extends PackageServiceProvider
 {
